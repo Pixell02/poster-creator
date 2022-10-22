77ceaf93b673fb66bbfb87f2caf742489feb931d
Pixel <nikodems1308@gmail.com>
2022-10-22 16:34:59 +0200

    changed location

--- save-file.js
+++
@@ -1,8 +0,0 @@
-const saveBtn = document.getElementById("save");
-saveBtn.addEventListener("click", function () {
-  domtoimage
-    .toBlob(document.getElementById("poster-edit"))
-    .then(function (blob) {
-      window.saveAs(blob, "my-node.png");
-    });
-});

8ad709144b3c972c4d2c8e7284c8ce4208f4dfbd
Pixel <nikodems1308@gmail.com>
2022-10-22 16:37:54 +0200

    added file

---
+++ js_files/creator-classes.js
@@ -0,0 +1,18 @@
+export class poster {
+    constructor(yourTeamName, yourTeamLogo, yourTeamPlayer, opponentTeamName, opponentTeamlogo,) {
+        this.yourTeamName = yourTeamName;
+        this.yourTeamLogo = yourTeamLogo;
+        this.yourTeamPlayer = yourTeamPlayer;
+        this.opponentTeamName = opponentTeamName;
+        this.opponentTeamLogo = opponentTeamLogo;
+    }
+}
+export class posterUI {
+
+    writeYourTeamName(){}
+    uploadYourLogo(){}
+    changeYourTeamNameColor(){}
+    uploadOpponentTeamLogo() {}
+
+
+}

b4288af743cce0722755d02d457d5ddecad7657c
Pixel <nikodems1308@gmail.com>
2022-11-11 13:02:00 +0100

    new listeners

--- js_files/creator.js
+++ js_files/creator.js
@@ -2,12 +2,11 @@ import { saveFile } from "./save-file.js";
 import { Edit, posterUI } from "./creator-classes.js";
 
 window.addEventListener("DOMContentLoaded", posterUI.loadImagesToSelect);
-
+let pickFont = "montserrat";
 const WIDTH = 700;
 const canvas = document.getElementById("poster-edit");
 canvas.width = WIDTH;
 canvas.height = WIDTH;
-console.log(canvas.height);
 const fabricCanvas = new fabric.Canvas(canvas);
 document.getElementsByClassName("canvas-container")[0].style.width = "700px";
 document.getElementsByClassName("canvas-container")[0].style.height = "700px";
@@ -28,7 +27,6 @@ function createBackgroundImage(fabricCanvas) {
 
   canvas.height = img.height * ratio;
   canvas.width = WIDTH;
-  console.log(canvas.height);
   fabricCanvas.width = WIDTH;
   fabricCanvas.height = img.height * ratio;
   img.onload = () => {
@@ -43,24 +41,54 @@ function createBackgroundImage(fabricCanvas) {
   };
 }
 
-
 // Writing name of your team
 
 const yourTeamInput = document.getElementById("team");
 
 yourTeamInput.addEventListener("keyup", (e) => {
-  
   posterUI.writeYourTeamName(e, yourTeamInput, fabricCanvas, WIDTH);
 });
 
 // Setting color of your team text
 
-const getColorInput = document.querySelector("#first-text-color")
+const getColorInput = document.querySelector("#first-text-color");
 
 getColorInput.addEventListener("change", (e) => {
-  Edit.getFirstColor(e, fabricCanvas, yourTeamInput);
-})
+  Edit.getFirstColor(e, yourTeamInput, fabricCanvas);
+});
+
+// Setting font of your team text
+
+const getFonts = document.querySelector("#font1");
+getFonts.onchange = (e) => {
+  Edit.getFont(e, yourTeamInput, fabricCanvas);
+};
+// Setting size of stroke on text
+
+const getStrokeSize = document.querySelector("#strokeSize1");
+getStrokeSize.addEventListener("change", (e) => {
+  Edit.setStroke(e, yourTeamInput, fabricCanvas);
+});
+
+// Setting color of stroke on text
+
+const getStrokeColor = document.querySelector("#strokeColor1");
+getStrokeColor.addEventListener("change", (e) => {
+  Edit.setColorStroke(e, yourTeamInput, fabricCanvas);
+});
+
+// Setting color of shadow on text
+
+const getShadowColor = document.querySelector("#ShadowColor1");
+getShadowColor.addEventListener("change", (e) => {
+  Edit.setColorShadow(e, yourTeamInput, fabricCanvas);
+});
+
+//
 // Writing city, date and hour
+// 
+
+
 
 const cityInput = document.getElementById("city");
 
@@ -68,15 +96,89 @@ cityInput.addEventListener("keyup", (e) => {
   posterUI.writeCity(e, cityInput, fabricCanvas, WIDTH);
 });
 
-// Writing opponents
+// Setting color of your team text
 
-const opponentInput = document.getElementById("opponent");
+const getCityColorInput = document.querySelector("#city-color");
+
+getCityColorInput.addEventListener("change", (e) => {
+  Edit.getSecondColor(e, cityInput, fabricCanvas);
+});
+
+// Setting font of your team text
+
+const getSecondFonts = document.querySelector("#font2");
+getSecondFonts.onchange = (e) => {
+  Edit.getSecondFont(e, cityInput, fabricCanvas);
+};
+// Setting size of stroke on text
+
+const getSecondStrokeSize = document.querySelector("#StrokeSize2");
+getSecondStrokeSize.addEventListener("change", (e) => {
+  Edit.setSecondStroke(e, cityInput, fabricCanvas);
+});
+
+// Setting color of stroke on text
+
+const getSecondStrokeColor = document.querySelector("#StrokeColor2");
+getSecondStrokeColor.addEventListener("change", (e) => {
+  Edit.setSecondColorStroke(e, cityInput, fabricCanvas);
+});
+
+// Setting color of shadow on text
+
+const getSecondShadowColor = document.querySelector("#ShadowColor2");
+getSecondShadowColor.addEventListener("change", (e) => {
+  Edit.setSecondColorShadow(e, cityInput, fabricCanvas);
+});
+
+// 
+// Writing opponents
+//
+const opponentInput = document.querySelector("#opponent");
 
 opponentInput.addEventListener("keyup", (e) => {
   posterUI.writeOpponentTeamName(e, opponentInput, fabricCanvas, WIDTH);
+}); 
+
+// Setting color of opponent text
+
+const opponentColor = document.querySelector("#opponent-team-color");
+
+opponentColor.addEventListener("change", (e) => {
+  posterUI.writeOpponentTeamName(e, opponentInput, fabricCanvas);
 });
 
+// Setting font of text
+
+const getThirdFonts = document.querySelector("#font3");
+getThirdFonts.onchange = (e) => {
+  Edit.getThirdFont(e, opponentInput, fabricCanvas);
+};
+
+// Setting size of stroke on text
+
+const getThirdStrokeSize = document.querySelector("#StrokeSize3");
+getThirdStrokeSize.addEventListener("change", (e) => {
+  Edit.setThirdStroke(e, opponentInput, fabricCanvas);
+});
+
+// Setting color of stroke on text
+
+const getThirdStrokeColor = document.querySelector("#StrokeColor3");
+getThirdStrokeColor.addEventListener("change", (e) => {
+  Edit.setThirdColorStroke(e, opponentInput, fabricCanvas);
+});
+
+// Setting color of shadow on text
+
+const getThirdShadowColor = document.querySelector("#ShadowColor3");
+getThirdShadowColor.addEventListener("change", (e) => {
+  Edit.setThirdColorShadow(e, opponentInput, fabricCanvas);
+});
+
+
 // More options
+
 let showFirstOption = false;
 const moreOptions1 = document.querySelector(".more-options1");
 moreOptions1.addEventListener("click", (e) => {
@@ -89,9 +191,9 @@ moreOptions1.addEventListener("click", (e) => {
     showFirstOption = false;
   }
 });
-  if(showFirstOption == true) {
-    Edit.getFirstColor();
-  }
+if (showFirstOption == true) {
+  Edit.getFirstColor();
+}
 const moreOptions2 = document.querySelector(".more-options2");
 moreOptions2.addEventListener("click", (e) => {
   if (document.querySelector("#city-text-color").style.display == "none") {
@@ -105,22 +207,21 @@ const moreOptions3 = document.querySelector(".more-options3");
 moreOptions3.addEventListener("click", (e) => {
   if (document.querySelector("#opponent-team-text").style.display == "none") {
     document.querySelector("#opponent-team-text").style.display = "initial";
-    
   } else {
     document.querySelector("#opponent-team-text").style.display = "none";
   }
 });
-  const opponentOption = document.querySelector("#opponent-team");
+const opponentOption = document.querySelector("#opponent-team");
 
-  opponentOption.addEventListener("change", (e) => {
-    let opponentLogo = opponentOption.value;
-    console.log(opponentLogo);
-    Edit.addOpponentLogo(opponentLogo, fabricCanvas);
-  });
+opponentOption.addEventListener("change", (e) => {
+  let opponentLogo = e.target.value;
+  console.log(opponentLogo);
+  Edit.addOpponentLogo(opponentLogo, fabricCanvas);
+});
 
-  const yourPlayerImage = document.querySelector("#your-player");
-  yourPlayerImage.addEventListener("change", (e) => {
-    const playerImageSrc = yourPlayerImage.value;
-    Edit.addYourPlayerImage(playerImageSrc, fabricCanvas);
-  });
+const yourPlayerImage = document.querySelector("#your-player");
+yourPlayerImage.addEventListener("change", (e) => {
+  const playerImageSrc = yourPlayerImage.value;
+  Edit.addYourPlayerImage(playerImageSrc, fabricCanvas);
+});
 saveFile();

c6b354b7a51c90afb7c7feda06d1b0207d0b1fa0
Pixel <nikodems1308@gmail.com>
2022-11-14 11:11:19 +0100

    deleted

--- generator-grafik.js
+++
@@ -1,9 +0,0 @@
-const creator = document.getElementById("create-poster");
-
-creator.addEventListener("click", openCreator);
-
-function openCreator(e) {
-  window.location.href = "kreator.html";
-
-  e.preventDefault();
-}
